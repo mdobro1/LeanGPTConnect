4c17801e3f2bc2018e6425ade1ca6e5bb29be148
Mykola Dobrochynskyy <mykola.dobrochynskyy@live.de>
2024-06-15 12:51:34 +0200

    vision demo refactoring

--- demo/vision/Listing_1_A.py
+++ demo/vision/Listing_1_A.py
@@ -1,21 +1,27 @@
-from openai import OpenAI
+import os
+import requests
+import json
 
-response = OpenAI().chat.completions.create(
-  model="gpt-4o",
-  messages=[
+api_key = os.getenv("OPENAI_API_KEY")
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4o",
+  "messages": [
     {
       "role": "user",
       "content": [
-        {"type": "text", "text": "What do you see in this image?"},
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": "https://bit.ly/img_city_1"
-          }
-        }
+          {"type": "text", "text": "What do you see in this image?"},
+          {"type": "image_url", "image_url": { "url": "https://bit.ly/img_city_1" }}
       ]
     }
-  ], max_tokens=1000
-)
+  ], "max_tokens": 1000
+}
 
-print(response.choices[0].message.content)
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+response_data = json.loads(response.text)
+
+print(response_data.get("choices")[0].get("message").get("content"))
